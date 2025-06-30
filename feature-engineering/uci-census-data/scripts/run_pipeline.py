4bb48172b37efc86edfe1ca9c13beb7f7dfb3258
nilays <shrimanwar92@gmail.com>
2025-06-30 23:02:33 +0530

    added zenml pipeline

--- feature-engineering/uci-census-data/scripts/run_pipeline.py
+++ feature-engineering/uci-census-data/scripts/run_pipeline.py
@@ -4,31 +4,45 @@ from validate_tfdv import validate_data
 import glob
 import filecmp
 import os
+from zenml import step, pipeline
 
-SCHEMA_UPDATE = True
+SCHEMA_UPDATE = False
 RAW_DIR = "../dataset/raw"
 RAW_FILES = glob.glob(f"{RAW_DIR}/*.csv")
 CLEAN_DIR = '../dataset/clean'
 OUTPUT_DIR = "../outputs"
 os.makedirs(CLEAN_DIR, exist_ok=True)
 
-def main():
+@step
+def preprocess_data_step():
     dcmp = filecmp.dircmp(RAW_DIR, CLEAN_DIR)
     has_matching_files = len(dcmp.left_only) == len(dcmp.right_only)
 
-    print(has_matching_files)
+    print("Do files match between raw and clean?", has_matching_files)
     
-    if has_matching_files == False:
+    if not has_matching_files:
         print("Preprocessing data...")
-        preprocess_data(RAW_FILES)
+        preprocess_data(RAW_FILES)  # this is your imported function
         print("Finished data preprocessing.")
-    
+
+@step
+def compute_stats_step():
     if SCHEMA_UPDATE == True:
         print("Computing baseline TFDV stats...")
         compute_tfdv_schema()
         print("Schema generated successfully.")
-    
-    #validate_data()
+
+@step
+def validate_new_data():
+    validate_data()
+
+
+@pipeline(enable_cache=False)
+def full_pipeline():
+    cleaned = preprocess_data_step()
+    schema = compute_stats_step()
+    #validate_new_data()  # you can pass inputs here if needed
 
 if __name__ == "__main__":
-    main()
+    full_pipeline()
+
